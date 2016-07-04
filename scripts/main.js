58ff8cf4c1108b882ba384581a58da4e3e1435f3
R Conner Howell <howell50@purdue.edu>
2016-07-04 21:43:40 +0200

    Init

---
+++ scripts/main.js
@@ -0,0 +1,120 @@
+// R. Conner Howell
+// Polygons 2016
+// Purpose: Mostly and art project.
+// Goal: Generate a bunch of polygons and create some cool art
+// Check support
+if (!SVG.supported)
+    alert("Your browser does not support SVG and this page will not work :'(");
+// Initialize svg with the screen bounds at the time of page load
+var svg = SVG('drawing').size(window.innerWidth, window.innerHeight);
+var Palette = (function () {
+    function Palette() {
+        this.randomize();
+    }
+    Palette.prototype.randomize = function () {
+        // Randomize which color array is the list
+        var len = Palette.lists.length;
+        var val = Math.floor(Math.random() * len) % len;
+        while (this.index == val) {
+            val = Math.floor(Math.random() * len) % len;
+        }
+        this.index = val;
+        this.list = Palette.lists[this.index];
+    };
+    Palette.lists = [
+        ["#8CD790", "#77AF9C", "#285943"],
+        ["#73628A", "#CBC5EA", "#313D5A"],
+        ["#59544B", "#7D8CA3", "#79A9D1"],
+        ["#546A7B", "#9EA3B0", "#FAE1DF"],
+        ["#FF4B3E", "#FFB20F", "#FFE548"],
+        ["#8D89A6", "#BFABCB", "#E6C0E9"]
+    ];
+    return Palette;
+}());
+// Point class so that 'number' types can be kept
+// The SVG.js library deals with strings, and I would prefer to have 'number' types stored
+var Point = (function () {
+    function Point(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    // toString exists for use in rendering the polygon with SVG.js
+    Point.prototype.toString = function () {
+        return this.x + "," + this.y;
+    };
+    return Point;
+}());
+// Polygon class to be used for generating all screen content
+var Polygon = (function () {
+    function Polygon(a, b, c, d) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        // Points are represented by a string "a,b"
+        // Turn point objects into a string recognizable by SVG.js
+        this.points = a.toString() + " " + b.toString() + " " + c.toString() + " " + d.toString();
+    }
+    Polygon.prototype.getPoints = function () {
+        return this.points;
+    };
+    return Polygon;
+}());
+// Box is composed of three polygons and seven points
+var Box = (function () {
+    function Box(svg, cx, cy, scale) {
+        // Small and Big are the differences that the point is offset by the scale
+        var small = (scale / 4);
+        var big = (scale / 2);
+        // Create the 7 points
+        // p1 is at the center and p2-p7 form a hexagon around the center
+        // I am not able to make an ascii visual unfortunately
+        var p1 = new Point(cx, cy);
+        var p2 = new Point(cx, cy - big);
+        var p3 = new Point(cx + big, cy - small);
+        var p4 = new Point(cx - big, cy - small);
+        var p5 = new Point(cx + big, cy + small);
+        var p6 = new Point(cx - big, cy + small);
+        var p7 = new Point(cx, cy + big);
+        // Polygons for the box
+        var left = new Polygon(p6, p1, p2, p4);
+        var right = new Polygon(p1, p5, p3, p2);
+        var top = new Polygon(p6, p7, p5, p1);
+        // Draw them with SVG.js
+        this.left = svg.polygon(left.getPoints()).fill('black').stroke({ width: 1 });
+        this.right = svg.polygon(right.getPoints()).fill('transparent').stroke({ width: 1 });
+        this.top = svg.polygon(top.getPoints()).fill('transparent').stroke({ width: 1 });
+    }
+    Box.prototype.changeColor = function (palette) {
+        this.left.fill(palette[0]);
+        this.right.fill(palette[1]);
+        this.top.fill(palette[2]);
+    };
+    return Box;
+}());
+// Constant variable
+var COLUMNS = 15;
+// Scale is the width over (2 * columns) because the boxes extend in the +x and -x direction by 2 * scale
+var scale = window.innerWidth / COLUMNS;
+var boxes = [];
+for (var i = 0; i < COLUMNS; i++) {
+    for (var j = 0; j < COLUMNS + 1; j++) {
+        if (i % 2 == 0) {
+            boxes[boxes.length] = new Box(svg, j * scale, i * (scale - (scale / 4)), scale);
+        }
+        else {
+            boxes[boxes.length] = new Box(svg, j * scale + (scale / 2), i * (scale - (scale / 4)), scale);
+        }
+    }
+}
+// Initialize color palette
+var palette = new Palette();
+// This adds an onclick function to the whole body
+// The function changes the color of the boxes
+document.getElementsByTagName("body")[0].onclick = function () {
+    palette.randomize();
+    for (var _i = 0, boxes_1 = boxes; _i < boxes_1.length; _i++) {
+        var box = boxes_1[_i];
+        box.changeColor(palette.list);
+    }
+};
---
+++ scripts/main.ts
@@ -0,0 +1,147 @@
+// R. Conner Howell
+// Polygons 2016
+// Purpose: Mostly and art project.
+// Goal: Generate a bunch of polygons and create some cool art
+
+
+// Required to use the SVG library
+declare var SVG:any;
+
+// Check support
+if (!SVG.supported) alert("Your browser does not support SVG and this page will not work :'(");
+
+// Initialize svg with the screen bounds at the time of page load
+let svg = SVG('drawing').size(window.innerWidth, window.innerHeight);
+
+class Palette {
+  private static lists = [
+    ["#8CD790", "#77AF9C", "#285943"],
+    ["#73628A", "#CBC5EA", "#313D5A"],
+    ["#59544B", "#7D8CA3", "#79A9D1"],
+    ["#546A7B", "#9EA3B0", "#FAE1DF"],
+    ["#FF4B3E", "#FFB20F", "#FFE548"],
+    ["#8D89A6", "#BFABCB", "#E6C0E9"]
+  ];
+  // The list of the current color scheme
+  public list: string[];
+  // This field is used so that it is not randomized to the same color
+  private index: number;
+  constructor() {
+    this.randomize();
+  }
+  randomize() {
+    // Randomize which color array is the list
+    let len = Palette.lists.length;
+    let val = Math.floor(Math.random() * len) % len;
+    while (this.index == val) {
+      val = Math.floor(Math.random() * len) % len;
+    }
+    this.index = val;
+    this.list = Palette.lists[this.index];
+  }
+}
+
+// Point class so that 'number' types can be kept
+// The SVG.js library deals with strings, and I would prefer to have 'number' types stored
+class Point {
+  private x: number;
+  private y: number;
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+  // toString exists for use in rendering the polygon with SVG.js
+  toString(): string {
+    return `${this.x},${this.y}`;
+  }
+}
+
+// Polygon class to be used for generating all screen content
+class Polygon {
+
+  // It has points a,b,c,d
+  // a ----- b
+  // |      |
+  // d ----- c
+  // Polygons are limited to 4 points for this project
+
+  // String containing all points in the format "a b c d" where a = "x,y" etc.
+  private points: string;
+  constructor(public a: Point, public b: Point, public c: Point, public d: Point) {
+    // Points are represented by a string "a,b"
+    // Turn point objects into a string recognizable by SVG.js
+    this.points = `${a.toString()} ${b.toString()} ${c.toString()} ${d.toString()}`;
+  }
+  getPoints(): string {
+    return this.points;
+  }
+}
+
+// Box is composed of three polygons and seven points
+class Box {
+  private left: any;
+  private right: any;
+  private top: any;
+  constructor(svg: any, cx: number, cy: number, scale: number) {
+
+    // Small and Big are the differences that the point is offset by the scale
+    let small = (scale / 4);
+    let big = (scale / 2);
+
+    // Create the 7 points
+    // p1 is at the center and p2-p7 form a hexagon around the center
+    // I am not able to make an ascii visual unfortunately
+    let p1 = new Point(cx, cy);
+    let p2 = new Point(cx, cy - big);
+    let p3 = new Point(cx + big, cy - small);
+    let p4 = new Point(cx - big, cy - small);
+    let p5 = new Point(cx + big, cy + small);
+    let p6 = new Point(cx - big, cy + small);
+    let p7 = new Point(cx, cy + big);
+
+    // Polygons for the box
+    let left = new Polygon(p6, p1, p2, p4);
+    let right = new Polygon(p1, p5, p3, p2);
+    let top = new Polygon(p6, p7, p5, p1);
+
+    // Draw them with SVG.js
+    this.left = svg.polygon(left.getPoints()).fill('black').stroke({width: 1});
+    this.right = svg.polygon(right.getPoints()).fill('transparent').stroke({width: 1});
+    this.top = svg.polygon(top.getPoints()).fill('transparent').stroke({width: 1});
+  }
+  changeColor(palette: string[]) {
+    this.left.fill(palette[0]);
+    this.right.fill(palette[1]);
+    this.top.fill(palette[2]);
+  }
+}
+
+// Constant variable
+let COLUMNS = 15;
+
+// Scale is the width over (2 * columns) because the boxes extend in the +x and -x direction by 2 * scale
+let scale = window.innerWidth / COLUMNS;
+
+let boxes: Array<Box> = [];
+
+for (let i = 0; i < COLUMNS; i++) {
+  for (let j = 0; j < COLUMNS + 1; j++) {
+    if (i % 2 == 0) {
+      boxes[boxes.length] = new Box(svg, j * scale, i * (scale - (scale  / 4)), scale);
+    } else {
+      boxes[boxes.length] = new Box(svg, j * scale + (scale  / 2), i * (scale - (scale  / 4)), scale);
+    }
+  }
+}
+
+// Initialize color palette
+let palette = new Palette();
+
+// This adds an onclick function to the whole body
+// The function changes the color of the boxes
+document.getElementsByTagName("body")[0].onclick = function(){
+  palette.randomize();
+  for (let box of boxes) {
+    box.changeColor(palette.list);
+  }
+}
